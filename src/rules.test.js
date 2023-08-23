7f8ef1120a6d5c3cfdd101e59caa45d104046690
Robin Malfait <malfait.robin@gmail.com>
2023-08-23 11:47:45 +0200

    add tests for the implemented rules

---
+++ src/rules.test.js
@@ -0,0 +1,154 @@
+import { describe, test, expect } from "vitest";
+import { checkValidity } from "./rules";
+
+describe("valid", () => {
+  test("a correct number is valid", () => {
+    expect(() => checkValidity({ content: "1" }, [], 0)).not.toThrowError();
+  });
+
+  test("a number with leading zeroes is valid", () => {
+    expect(() => checkValidity({ content: "001" }, [], 0)).not.toThrowError();
+  });
+
+  test("a number with a space after it is valid", () => {
+    expect(() => checkValidity({ content: "1 " }, [], 0)).not.toThrowError();
+  });
+
+  test("a number with a space and a message after it is valid", () => {
+    expect(() =>
+      checkValidity({ content: "1 a message can go here" }, [], 0),
+    ).not.toThrowError();
+  });
+});
+
+describe("invalid", () => {
+  test("a non-number message is invalid", () => {
+    expect(() =>
+      checkValidity({ content: "something" }, [], 0),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"Message \\"something\\" does not start with a number."',
+    );
+  });
+
+  test("a character before a number is invalid", () => {
+    expect(() =>
+      checkValidity({ content: " 1" }, [], 0),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"Message \\" 1\\" does not start with a number."',
+    );
+  });
+
+  test("a non-space character after a number is invalid", () => {
+    expect(() =>
+      checkValidity({ content: "1?" }, [], 0),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"Extra character \\"?\\" found after number \\"1\\"."',
+    );
+  });
+
+  test("posting the wrong number is invalid", () => {
+    expect(() =>
+      checkValidity({ content: "2" }, [], 0),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"Wrong number, expected \\"1\\" got \\"2\\"."',
+    );
+  });
+
+  test("posting the same number as the previous number is invalid (double post)", () => {
+    expect(() =>
+      checkValidity({ content: "2" }, [], 2),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"Wrong number, expected \\"3\\" got \\"2\\"."',
+    );
+  });
+
+  test("posting within 5 messages of your last message is invalid (4)", () => {
+    expect(() =>
+      checkValidity(
+        { content: "6", author: { id: "alice", displayName: "Alice" } },
+        [
+          { content: "1", author: { id: "alice", displayName: "Alice" } },
+          { content: "2", author: { id: "bob", displayName: "Bob" } },
+          { content: "3", author: { id: "charlie", displayName: "Charlie" } },
+          { content: "4", author: { id: "dave", displayName: "Dave" } },
+          { content: "5", author: { id: "erin", displayName: "Erin" } },
+        ],
+        5,
+      ),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"There are only \\"4\\" messages between this message and the last message from \\"Alice\\"."',
+    );
+  });
+
+  test("posting within 5 messages of your last message is invalid (3)", () => {
+    expect(() =>
+      checkValidity(
+        { content: "6", author: { id: "bob", displayName: "Bob" } },
+        [
+          { content: "1", author: { id: "alice", displayName: "Alice" } },
+          { content: "2", author: { id: "bob", displayName: "Bob" } },
+          { content: "3", author: { id: "charlie", displayName: "Charlie" } },
+          { content: "4", author: { id: "dave", displayName: "Dave" } },
+          { content: "5", author: { id: "erin", displayName: "Erin" } },
+        ],
+        5,
+      ),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"There are only \\"3\\" messages between this message and the last message from \\"Bob\\"."',
+    );
+  });
+
+  test("posting within 5 messages of your last message is invalid (2)", () => {
+    expect(() =>
+      checkValidity(
+        { content: "6", author: { id: "charlie", displayName: "Charlie" } },
+        [
+          { content: "1", author: { id: "alice", displayName: "Alice" } },
+          { content: "2", author: { id: "bob", displayName: "Bob" } },
+          { content: "3", author: { id: "charlie", displayName: "Charlie" } },
+          { content: "4", author: { id: "dave", displayName: "Dave" } },
+          { content: "5", author: { id: "erin", displayName: "Erin" } },
+        ],
+        5,
+      ),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"There are only \\"2\\" messages between this message and the last message from \\"Charlie\\"."',
+    );
+  });
+
+  test("posting within 5 messages of your last message is invalid (1)", () => {
+    expect(() =>
+      checkValidity(
+        { content: "6", author: { id: "dave", displayName: "Dave" } },
+        [
+          { content: "1", author: { id: "alice", displayName: "Alice" } },
+          { content: "2", author: { id: "bob", displayName: "Bob" } },
+          { content: "3", author: { id: "charlie", displayName: "Charlie" } },
+          { content: "4", author: { id: "dave", displayName: "Dave" } },
+          { content: "5", author: { id: "erin", displayName: "Erin" } },
+        ],
+        5,
+      ),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"There are only \\"1\\" messages between this message and the last message from \\"Dave\\"."',
+    );
+  });
+
+  test("posting within 5 messages of your last message is invalid (0)", () => {
+    expect(() =>
+      checkValidity(
+        { content: "6", author: { id: "erin", displayName: "Erin" } },
+        [
+          { content: "1", author: { id: "alice", displayName: "Alice" } },
+          { content: "2", author: { id: "bob", displayName: "Bob" } },
+          { content: "3", author: { id: "charlie", displayName: "Charlie" } },
+          { content: "4", author: { id: "dave", displayName: "Dave" } },
+          { content: "5", author: { id: "erin", displayName: "Erin" } },
+        ],
+        5,
+      ),
+    ).toThrowErrorMatchingInlineSnapshot(
+      '"There are only \\"0\\" messages between this message and the last message from \\"Erin\\"."',
+    );
+  });
+});
