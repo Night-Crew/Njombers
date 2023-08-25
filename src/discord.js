7adab2955502fcfab0f808352e8dd33cd635c9a8
Robin Malfait <malfait.robin@gmail.com>
2023-08-25 15:28:12 +0200

    a few more error messages

--- src/discord.js
+++ src/discord.js
@@ -167,6 +167,7 @@ export async function initClient() {
           `Kom eens scheidsrechterke spelen they said, it will be fun they said. En dan komt ge dit tegen.`,
           `Ik heb het al gezegd, ik heb het al gezegd, ik heb het al 1000 keer gezegd. Maar nee, ${message.author} moet weer zijn eigen zin doen. En dan krijgde dit.`,
           `Ge hebt hier gene PhD voor nodig om te zien dat dit niet klopt. ${message.author} toch!`,
+          `Hup, weg sfeer. \`${errors.length}\` fouten, ja hallo.`,
         ];
 
         response = `${
